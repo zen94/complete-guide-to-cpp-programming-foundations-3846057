b01daf37aa6075e593fd94467c0f3a03eff76130
Lim Zhen En <53658754+zen94@users.noreply.github.com>
2025-03-17 14:26:04 +0000

    added a comment

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 00_03
+// Exercise 00_03 with modifications
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
